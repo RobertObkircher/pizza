823922a29b30998d3d0d765ead8d3e1918b20742
Robert Obkircher <robert.obkircher@gmail.com>
2019-01-17 11:43:37 +0100

    parse files and compute possible shapes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,110 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int width;
+    int height;
+} Shape;
 
 int main() {
-    printf("Hello, World!\n");
+
+    int file_index = 0;
+    const char *files[] = {
+            "inputs/a_example.in",
+            "inputs/b_small.in",
+            "inputs/c_medium.in",
+            "inputs/d_big.in",
+    };
+
+    printf("file: %s\n", files[file_index]);
+    FILE *file = fopen(files[file_index], "r");
+    if (!file) {
+        printf("Unable to open file!");
+        exit(1);
+    }
+
+    int R, C, L, H;
+    fscanf(file, "%d %d %d %d\n", &R, &C, &L, &H);
+
+    char *pizza = malloc(R * C * sizeof(char));
+    {
+        for (int row = 0; row < R; ++row) {
+            for (int col = 0; col < C; ++col) {
+                pizza[row * C + col] = (char) getc(file);
+            }
+            getc(file);
+        }
+
+        printf("RCLH %d %d %d %d\n", R, C, L, H);
+//        for (int row = 0; row < R; ++row) {
+//            for (int col = 0; col < C; ++col) {
+//                printf("%c", *(pizza + row * C + col));
+//            }
+//            printf("\n");
+//        }
+    }
+
+    int number_of_shapes = 0;
+    Shape *shapes;
+    {
+        for (int x = 1; x <= H; ++x) {
+            for (int y = 1; y <= H; ++y) {
+                if (x * y <= H) {
+                    ++number_of_shapes;
+                }
+            }
+        }
+        shapes = malloc(sizeof(Shape) * number_of_shapes);
+        number_of_shapes = 0;
+        for (int x = 1; x <= H; ++x) {
+            for (int y = 1; y <= H; ++y) {
+                if (x * y <= H) {
+                    shapes[number_of_shapes++] = (Shape) {.width = x, .height=y};
+                }
+            }
+        }
+        printf("%d Shapes\n", number_of_shapes);
+//        for (int i = 0; i < number_of_shapes; ++i) {
+//            printf("%d %d\n", shapes[i].width, shapes[i].height);
+//        }
+    }
+
+    long long *possible_shapes = malloc(R * C * sizeof(long long));
+    {
+        for (int pizza_row = 0; pizza_row < R; ++pizza_row) {
+            for (int pizza_col = 0; pizza_col < C; ++pizza_col) {
+                int pizza_index = pizza_row * C + pizza_col;
+                possible_shapes[pizza_index] = 0;
+                for (int i = 0; i < number_of_shapes; ++i) {
+                    Shape shape = shapes[i];
+                    if (shape.width + pizza_col > C || shape.height + pizza_row > R)
+                        continue;
+                    int mushrooms = 0;
+                    for (int y = 0; y < shape.height; ++y) {
+                        for (int x = 0; x < shape.width; ++x) {
+                            if (pizza[pizza_index + y * C + x] == 'M') {
+                                ++mushrooms;
+                            }
+                        }
+                    }
+                    if (mushrooms >= L && shape.width * shape.height - mushrooms >= L) {
+                        possible_shapes[pizza_index] |= 1 << i;
+                    }
+                }
+            }
+        }
+
+//        for (int pizza_row = 0; pizza_row < R; ++pizza_row) {
+//            for (int pizza_col = 0; pizza_col < C; ++pizza_col) {
+//                printf("%8x ", possible_shapes[pizza_row * C + pizza_col]);
+//            }
+//            printf("\n");
+//        }
+    }
+
+
+    free(shapes);
+    free(pizza);
     return 0;
 }
