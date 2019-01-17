498abbccc480f7ec6f99c1c270baa44fc4206799
Robert Obkircher <robert.obkircher@gmail.com>
2019-01-17 12:12:44 +0100

    sketch of the algorithm

--- src/main.c
+++ src/main.c
@@ -7,6 +7,15 @@ typedef struct {
     int height;
 } Shape;
 
+int bit_count(unsigned long long n) {
+    int counter = 0;
+    while (n) {
+        ++counter;
+        n &= (n - 1);
+    }
+    return counter;
+}
+
 int main() {
 
     int file_index = 0;
@@ -101,6 +110,72 @@ int main() {
 //            }
 //            printf("\n");
 //        }
+    }
+
+    /*
+     for all shapes
+       compute number of shapes to the right
+       compute number of shapes below
+       the border counts as num_shapes possible shapes
+     pick shape with largest sum of neighbours
+     */
+    for (int pizza_row = 0; pizza_row < R; ++pizza_row) {
+        for (int pizza_col = 0; pizza_col < C; ++pizza_col) {
+            int pizza_index = pizza_row * C + pizza_col;
+            if (!possible_shapes[pizza_index])
+                continue;
+            Shape *best_shape = NULL;
+            int best_shape_score = -1;
+            for (int i = 0; i < number_of_shapes; ++i) {
+                if (!possible_shapes[pizza_index] & (1 << i))
+                    continue;
+
+                Shape *shape = &shapes[i];
+                int score = 0;
+
+                if (pizza_row == 0) {
+                    score += shape->width;
+                }
+                if (pizza_col == 0) {
+                    score += shape->height;
+                }
+
+                if (shape->width + pizza_col == C) {
+                    score += shape->height;
+                } else {
+                    unsigned long long neighbours = 0;
+                    for (int y = 0; y < shape->height; ++y) {
+                        neighbours |= possible_shapes[pizza_index + y * C + shape->width];
+                    }
+                    score += bit_count(neighbours);
+                }
+
+                if (shape->height + pizza_row == C) {
+                    score += shape->width;
+                } else {
+                    unsigned long long neighbours = 0;
+                    for (int x = 0; x < shape->width; ++x) {
+                        neighbours |= possible_shapes[pizza_index + x + shape->height * C];
+                    }
+                    score += bit_count(neighbours);
+                }
+
+                if (score > best_shape_score) {
+                    best_shape_score = score;
+                    best_shape = shape;
+                }
+            }
+
+            if (best_shape) {
+                // TODO store shape in list
+                printf("crwh: %d %d %d %d\n", pizza_col, pizza_row, best_shape->width, best_shape->height);
+                for (int y = 0; y < best_shape->height; ++y) {
+                    for (int x = 0; x < best_shape->width; ++x) {
+                        possible_shapes[pizza_index + y * C + x] = 0;
+                    }
+                }
+            }
+        }
     }
 
 
